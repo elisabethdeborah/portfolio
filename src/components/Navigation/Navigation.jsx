3275948bd20018e2a7d516861e67546ac3ff40d1
elisabethdeborah <elisabeth_deborah@hotmail.com>
2022-12-13 22:28:24 +0100

    change order of nav links

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,15 +4,16 @@ import clsx from 'clsx';
 
 const Navigation = ({ data, isStuck }) => {
 	let mapData = Object.keys(data);
-	let sorted = mapData.reverse();
+	let newArray = mapData.splice(-2, 2);
+	newArray = [...newArray, ...mapData];
 
 	return (
 		<section className={clsx(styles.navWrapper, {[styles.stuck]: isStuck})}>
 			<div className={styles.navBar}>
 				<ul className={styles.navList}>
-					{sorted.map((x, index) => {
+					{newArray.map((x, index) => {
 						return (
-							x !== 'faerdigheter' && x !== 'cvRegular' && x !== 'om_mig' && (
+							x !== 'faerdigheter' && x !== 'faerdigheterObjs' && x !== 'cvRegular' && (
 							<li key={`nav-link-${index+1}`} className={styles.navListItem}>
 								<a href={`#${x}`} className={styles.navLink}>{x.split('_').join(' ').split('ae').join('ä')}</a>
 							</li>)
